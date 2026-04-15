bc1b8621bc7024d7c31579709bfc055fc0e5b2f3
Rwanbt <barat.erwan@gmail.com>
2026-04-15 12:11:37 +0200

    Feat: flowchart & main.h

---
+++ main.h
@@ -0,0 +1,40 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+/* Prompt & Input */
+void display_prompt(void);
+charread_input(void);
+
+/* Parser */
+char **parse_input(charinput);
+void free_argv(char argv);
+
+/* Path */
+charfind_command(char *command);
+char get_path_dirs(void);
+
+/* Executor */
+int execute_command(char argv, char env);
+
+/* Built-ins */
+int is_builtin(charcommand);
+int execute_builtin(char argv, char env);
+int builtin_exit(char argv);
+int builtin_env(char env);
+
+/* Utils */
+char_strdup(char str);
+int _strlen(charstr);
+char _strcat(chardest, char src);
+int _strcmp(chars1, char s2);
+
+#endif /* SHELL_H */
