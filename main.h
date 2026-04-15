e38d298dc212fd0070c1778af40d682328f8df04
David Lengellé <david.lengelle.1.10@gmail.com>
2026-04-15 22:06:39 +0200

    Refactor: remove unnecessary comment for libraries in main.h

--- main.h
+++ main.h
@@ -1,7 +1,6 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-/* Libraries */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -38,4 +37,4 @@ char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 
-#endif /* SHELL_H */
+#endif
